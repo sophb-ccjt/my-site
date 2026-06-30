34d691efecc06a3c6dabf8fa82662e2aad97037a
Sophia Blair <sophb.ccjt@proton.me>
2026-06-30 07:51:34 -0300

    testing services

--- backend/apis/themes.js
+++ backend/apis/themes.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 export default async function handler(request, response) {
-    fs.readdir("/frontend/assets/themes", (err, files) => {
+    response.status(200).text('WIP!');
+    fs.readdir("/assets/themes", (err, files) => {
         if (err) console.error(err);
         else {
             files.sort((a, b) => {
